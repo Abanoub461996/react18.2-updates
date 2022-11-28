9cbd4e317e83d462366fcee04a273290f3ffd79d
Abanoub <pophtc.aa@gmail.com>
2022-11-28 15:51:34 +0200

    empty react app with bootstrap and navbar component

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+// COMPONENTS
+
+import NavbarComponent from './Views/NavBar/Navbar';
+
+
+function App() {
+  return (<>
+  <NavbarComponent />
+  <div className="App" />
+  </>
+    
+  );
+}
+
+export default App;
